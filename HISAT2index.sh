b6e05203868c99e972f714b6919976b0515e84ee
Mira Sohn <tonton07@gmail.com>
2020-11-04 17:28:18 -0500

    bash file executing hisat2-build indexer

---
+++ HISAT2index.sh
@@ -0,0 +1,22 @@
+#! bin/bash
+
+
+
+cd index_files
+
+ref_genome="hg19.fa" 
+
+
+hisat2-build -f -o 4 -t 10 --localoffrate 3 --localftabchars 6 -p 8 --seed 67 $ref_genome "index" 
+
+cd ..
+
+
+# Basic command line: hisat2-build [options] <reference_in> <ht2_base>
+# -f ~ --seed: options
+# "hg19.fa": <reference_in> 
+# "index": <ht2_base> 
+
+
+
+
